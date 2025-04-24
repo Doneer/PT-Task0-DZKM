ec3256f482f4240cfb7eb7bb55ed6ffa0bd8b3fe
Kuzma Martysiuk <253854@ude.p.lodz.pl>
2025-04-24 16:35:47 +0200

    Updated Catalog repository

--- Library.Data/Repositories/CatalogRepository.cs
+++ Library.Data/Repositories/CatalogRepository.cs
@@ -1,33 +1,53 @@
-﻿using Library.Data.Interfaces;
-using Library.Data.Models;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Library.Data.Interfaces;
+using Library.Data.Models;
 
 namespace Library.Data.Repositories
 {
     public class CatalogRepository : ICatalogRepository
     {
         private readonly List<Book> _books;
+        private readonly IModelFactory _modelFactory;
 
         public CatalogRepository(List<Book> initialBooks = null)
         {
             _books = initialBooks ?? new List<Book>();
+            _modelFactory = new Factories.ModelFactory();
         }
 
-        public IEnumerable<Book> GetAllBooks() => _books.ToList();
+        public IEnumerable<IBook> GetAllBooks() => _books.Cast<IBook>().ToList();
 
-        public Book GetBookById(string isbn) => _books.FirstOrDefault(b => b.ISBN == isbn);
+        public IBook GetBookById(string isbn) => _books.FirstOrDefault(b => b.ISBN == isbn);
 
-        public void AddBook(Book book)
+        public void AddBook(IBook book)
         {
             if (_books.Any(b => b.ISBN == book.ISBN))
             {
                 throw new ArgumentException($"Book with ISBN {book.ISBN} already exists.");
             }
-            _books.Add(book);
+
+            if (book is Book concreteBook)
+            {
+                _books.Add(concreteBook);
+            }
+            else
+            {
+                _books.Add(new Book
+                {
+                    ISBN = book.ISBN,
+                    Title = book.Title,
+                    Author = book.Author,
+                    Publisher = book.Publisher,
+                    PublicationYear = book.PublicationYear,
+                    Genre = book.Genre,
+                    Description = book.Description
+                });
+            }
         }
 
-        public void UpdateBook(Book book)
+        public void UpdateBook(IBook book)
         {
             var existingBook = _books.FirstOrDefault(b => b.ISBN == book.ISBN);
             if (existingBook == null)
@@ -36,7 +56,24 @@ namespace Library.Data.Repositories
             }
 
             _books.Remove(existingBook);
-            _books.Add(book);
+
+            if (book is Book concreteBook)
+            {
+                _books.Add(concreteBook);
+            }
+            else
+            {
+                _books.Add(new Book
+                {
+                    ISBN = book.ISBN,
+                    Title = book.Title,
+                    Author = book.Author,
+                    Publisher = book.Publisher,
+                    PublicationYear = book.PublicationYear,
+                    Genre = book.Genre,
+                    Description = book.Description
+                });
+            }
         }
 
         public void DeleteBook(string isbn)
