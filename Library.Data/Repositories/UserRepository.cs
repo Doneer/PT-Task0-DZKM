ba2857f5a2951859b077107fe72ca922b893d095
Kuzma Martysiuk <253854@ude.p.lodz.pl>
2025-04-24 16:35:03 +0200

    Updated userrepository

--- Library.Data/Repositories/UserRepository.cs
+++ Library.Data/Repositories/UserRepository.cs
@@ -1,33 +1,52 @@
-﻿using Library.Data.Interfaces;
-using Library.Data.Models;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Library.Data.Interfaces;
+using Library.Data.Models;
 
 namespace Library.Data.Repositories
 {
     public class UserRepository : IUserRepository
     {
         private readonly List<User> _users;
+        private readonly IModelFactory _modelFactory;
 
         public UserRepository(List<User> initialUsers = null)
         {
             _users = initialUsers ?? new List<User>();
+            _modelFactory = new Factories.ModelFactory();
         }
 
-        public IEnumerable<User> GetAllUsers() => _users.ToList();
+        public IEnumerable<IUser> GetAllUsers() => _users.Cast<IUser>().ToList();
 
-        public User GetUserById(int id) => _users.FirstOrDefault(u => u.Id == id);
+        public IUser GetUserById(int id) => _users.FirstOrDefault(u => u.Id == id);
 
-        public void AddUser(User user)
+        public void AddUser(IUser user)
         {
             if (_users.Any(u => u.Id == user.Id))
             {
                 throw new ArgumentException($"User with ID {user.Id} already exists.");
             }
-            _users.Add(user);
+
+            if (user is User concreteUser)
+            {
+                _users.Add(concreteUser);
+            }
+            else
+            {
+                _users.Add(new User
+                {
+                    Id = user.Id,
+                    Name = user.Name,
+                    Email = user.Email,
+                    PhoneNumber = user.PhoneNumber,
+                    Type = user.Type,
+                    RegistrationDate = user.RegistrationDate
+                });
+            }
         }
 
-        public void UpdateUser(User user)
+        public void UpdateUser(IUser user)
         {
             var existingUser = _users.FirstOrDefault(u => u.Id == user.Id);
             if (existingUser == null)
@@ -36,7 +55,23 @@ namespace Library.Data.Repositories
             }
 
             _users.Remove(existingUser);
-            _users.Add(user);
+
+            if (user is User concreteUser)
+            {
+                _users.Add(concreteUser);
+            }
+            else
+            {
+                _users.Add(new User
+                {
+                    Id = user.Id,
+                    Name = user.Name,
+                    Email = user.Email,
+                    PhoneNumber = user.PhoneNumber,
+                    Type = user.Type,
+                    RegistrationDate = user.RegistrationDate
+                });
+            }
         }
 
         public void DeleteUser(int id)
